d05b0da0ee2408abdc46378bbb1def379dbe5535
TheCJGCJG <charlesgillham@me.com>
2014-09-07 16:50:40 +0100

    Updated View.php

--- view.php
+++ view.php
@@ -1,12 +1,23 @@
 <?php
 require 'configs.php';
 
-$file = file_get_contents($encrypted_storage_folder . $_GET['img']);
-$key = $_GET['k'];
-$file = decrypt($file, $key);
+if(!isset($_GET['img'])) {
+	die('Enter a Valid IMAGE ID');
+}
 
+
+if (isset($_GET['k'])) {
+	$file = file_get_contents($encrypted_storage_folder . $_GET['img']);
+	$key = $_GET['k'];
+	$file = decrypt($file, $key);
+	$enc = 1;
+} else {
+	$enc = 0;
+	$file = file_get_contents($file_storage_folder . $_GET['img']);
+}
 header("Content-type: image/png");
+getImageStatsForHeaders($_GET['img']);
 echo $file;
-
+logView($_GET['img'], $enc);
 exit;
 ?>
