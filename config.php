348cd564b5dc094c0de763f07f0ff71e3d762707
Matt Kent <mattkent@infinity-six.com>
2014-10-11 23:41:07 +0100

    Re-organised file, cleaner layout.

--- config.php
+++ config.php
@@ -1,27 +1,27 @@
 <?php
 $sql = array(
-	'server' => 'localhost',
+	'server'   => 'localhost',
 	'username' => 'ScreenShottr',
 	'password' => 'PASSWORD',
 	'database' => 'ScreenShottr',
-	'port' => '3306'
+	'port'     => '3306'
 );
 $config = array(
-	'hashing_algorithm' => 'ripemd160',
-	'encryption_algorithm' => MCRYPT_RIJNDAEL_256,
-	'temp_directory' => '/var/www/html/ScreenShottr/tmp/',
-	'encrypted_directory' => '/var/www/html/ScreenShottr/encrypted/',
-	'unencrypted_directory' => '/var/www/html/ScreenShottr/i/',
-	'encrypted_URL' => 'https://www.screenshottr.us/v/{key}/{file}',
-	'unencrypted_URL' => 'https://www.screenshottr.us/{file}',
-	'pravius_enabled' => true,
-	'Encryption_Key_Length' => '32',
-	'secret_key_length' => '64',
-	'filenameLength' => '32',
-	'twitter_Encrypted_URL' => 'https://www.screenshottr.us/v/noBot/{key}/{file}',
+	'hashing_algorithm'       => 'ripemd160',
+	'encryption_algorithm'    => MCRYPT_RIJNDAEL_256,
+	'temp_directory'          => '/var/www/html/ScreenShottr/tmp/',
+	'encrypted_directory'     => '/var/www/html/ScreenShottr/encrypted/',
+	'unencrypted_directory'   => '/var/www/html/ScreenShottr/i/',
+	'encrypted_URL'           => 'https://www.screenshottr.us/v/{key}/{file}',
+	'unencrypted_URL'         => 'https://www.screenshottr.us/{file}',
+	'pravius_enabled'         => TRUE,
+	'Encryption_Key_Length'   => '32',
+	'secret_key_length'       => '64',
+	'filenameLength'          => '32',
+	'twitter_Encrypted_URL'   => 'https://www.screenshottr.us/v/noBot/{key}/{file}',
 	'twitter_Unencrypted_URL' => 'https://www.screenshottr.us/{file}/noBot',
-	'cloudflare_enabled' => false,
-	'twitter_card_ip' => array(
+	'cloudflare_enabled'      => FALSE,
+	'twitter_card_ip'         => array(
 		'199.16.156.124',
 		'199.16.156.125',
 		'199.16.156.126',
